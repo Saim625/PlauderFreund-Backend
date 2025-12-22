7d6be02426c62881668d948b36b84a940bbd95ad
saim625 <saimsaeed625@gmail.com>
2025-12-22 22:47:26 +0500

    import fix

--- src/services/realtimeService.js
+++ src/services/realtimeService.js
@@ -4,7 +4,7 @@ import logger from "../utils/logger.js";
 import { OPENAI_API_KEY, OPENAI_REALTIME_API } from "../config/env.js";
 import { greetingStore } from "../state/greetingStore.js";
 import PersonalityConfig from "../models/PersonalityConfig.js";
-import { buildOpenAIPrompt } from "./buildopenAiPrompt.js";
+import { buildOpenAIPrompt } from "./buildOpenAiPrompt.js";
 
 /**
  * Connect to OpenAI Realtime API
